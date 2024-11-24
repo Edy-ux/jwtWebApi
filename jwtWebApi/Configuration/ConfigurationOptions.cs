1b76c5b0d4a54e6080b15826dfb0164fd4164fe1
Ednei Lopes <70007086+Edy-ux@users.noreply.github.com>
2024-11-23 23:47:37 -0300

    update configurationOptions.

--- jwtWebApi/Configuration/ConfigurationOptions.cs
+++ jwtWebApi/Configuration/ConfigurationOptions.cs
@@ -4,5 +4,5 @@ public class ConfigurationOptions
 {
     public const string JWT = "JWT";
     public string Secret_Key { get; set; } = String.Empty;
-    public string Isuer { get; set; } = String.Empty;
+    public string Issuer { get; set; } = String.Empty;
 }
